c3a235494fa4e2aca91cadf94b3c5890baa7734e
Neil Smith <neil.git@njae.me.uk>
2018-09-17 17:25:25 +0100

    Haskell version using megaparsec

--- src/task1/task1-mpc.hs
+++ src/task1/task1-mpc.hs
@@ -1,4 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Data.List (foldl')        -- import the strict fold
+
+import Data.Text (Text)
+-- import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+import Data.Void (Void)
+
+import Text.Megaparsec -- hiding (State)
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+import qualified Control.Applicative as CA
+
 
 -- number of steps
 type Distance = Int
@@ -21,39 +35,19 @@ data Instruction =   Forward Distance
     deriving (Show, Eq)
 
 
-
 main :: IO ()
 main = do 
-        instruction_text <- readFile "data/01-mowmaster.txt"
-        print $ part1 instruction_text
-        print $ part2 instruction_text
+        instruction_text <- TIO.readFile "data/01-mowmaster.txt"
+        let instructions = successfulParse instruction_text
+        print $ part1 instructions
+        print $ part2 instructions
 
-part1 :: String -> Int
-part1 = length . filter (not . isComment) . lines
+part1 :: [Instruction] -> Int
+part1 = length 
 
-part2 :: String -> Int
-part2 instruction_text = finalDistance $ executeAll instructions
-    where instructions = readInstructions instruction_text
-          executeAll = foldl' execute initialMowmaster
-          
-
--- Is this line a comment?
-isComment :: String -> Bool
-isComment ('#':_) = True
-isComment _       = False
-
-
--- Extract the steps from the input string.
-readInstructions :: String -> [Instruction]
-readInstructions = (map readInstruction) . lines
-
-readInstruction :: String -> Instruction
-readInstruction i = readInstruction' (head i) (tail i)
-    where readInstruction' 'F' s = Forward (read s)
-          readInstruction' 'C' _ = Clockwise
-          readInstruction' 'A' _ = Anticlockwise
-          readInstruction'  _  t = Comment t
-
+part2 :: [Instruction] -> Int
+part2 instructions = finalDistance $ executeAll instructions
+    where executeAll = foldl' execute initialMowmaster
 
 initialMowmaster = Mowmaster North (0, 0)
 
@@ -69,8 +63,7 @@ execute :: Mowmaster -> Instruction -> Mowmaster
 execute m (Forward s)    = m {position  = forward s (direction m) (position m)}
 execute m  Clockwise     = m {direction = turnCW (direction m)}
 execute m  Anticlockwise = m {direction = turnACW (direction m)}
-execute m (Comment _)    = m
-
+execute m  _ = m
 
 -- Move in the current direction
 forward :: Int -> Direction -> Position -> Position
@@ -78,6 +71,7 @@ forward s North (e, n) = (e, n+s)
 forward s South (e, n) = (e, n-s)
 forward s West  (e, n) = (e-s, n)
 forward s East  (e, n) = (e+s, n)
+
 
 -- | a `succ` that wraps 
 turnCW :: (Bounded a, Enum a, Eq a) => a -> a 
@@ -88,3 +82,32 @@ turnCW dir | dir == maxBound = minBound
 turnACW :: (Bounded a, Enum a, Eq a) => a -> a
 turnACW dir | dir == minBound = maxBound
             | otherwise       = pred dir
+
+
+-- Parse the input file
+
+type Parser = Parsec Void Text
+
+-- treat comment lines as whitespace
+sc :: Parser ()
+sc = L.space space1 lineComment CA.empty
+    where lineComment = L.skipLineComment "#"
+
+lexeme  = L.lexeme sc
+integer = lexeme L.decimal
+symb = L.symbol sc
+
+-- instructions is some optional space followed by many instructions
+instrsP = optional sc *> many instrP
+instrP = forwardP <|> cwP <|> acwP
+
+-- parse each instruction
+forwardP = Forward <$> (symb "F" *> integer)
+cwP = Clockwise <$ symb "C"
+acwP = Anticlockwise <$ symb "A"
+
+successfulParse :: Text -> [Instruction]
+successfulParse input = 
+        case parse instrsP "input" input of
+                Left  _error -> [] -- TIO.putStr $ T.pack $ parseErrorPretty err
+                Right instrs  -> instrs
